b8d38bc73e3cbd5b78e45172f6d9035a1743d789
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-09-27 23:22:14 +0530

    add clerk-auth

--- app/documents/[id]/page.tsx
+++ app/documents/[id]/page.tsx
@@ -1,6 +1,3 @@
-import { Editor } from "@/components/editor/Editor"
-import Header from "@/components/Header"
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 
 const Document = () => {
     return (
